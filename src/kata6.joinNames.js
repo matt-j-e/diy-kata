5931a31c544b5abfb482ede24a0c49f29aa37eec
mattedwards <the.matt.edwards@googlemail.com>
2021-01-05 21:53:51 +0000

    completed joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    let s = '';
+    namesObj.map((obj, i) => {
+        if(i === namesObj.length - 1) {
+            s += obj.name;
+        } else if(i === namesObj.length - 2) {
+            s += obj.name + ' & ';
+        } else {
+            s += obj.name + ', ';
+        }
+    });
+    return s;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,27 @@
 const { joinNames } = require("../src");
 
+const arr1 = [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}];
+const arr2 = [
+  {name: 'Hollie'}, 
+  {name: 'Holly'}, 
+  {name: 'Secret James'},
+  {name: 'Bowling Alley James'},
+  {name: 'Jenny'},
+  {name: 'Jo'},
+  {name: 'Kallum'},
+  {name: 'Matt'},
+  {name: 'Miguel'},
+  {name: 'Nina'},
+  {name: 'Peechaya'}
+];
+const a2z = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const arr3 = [];
+a2z.split('').map(l => arr3.push({name: l}));
+
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames(arr1)).toBe('Bart, Lisa & Maggie');
+    expect(joinNames(arr2)).toBe('Hollie, Holly, Secret James, Bowling Alley James, Jenny, Jo, Kallum, Matt, Miguel, Nina & Peechaya');
+    expect(joinNames(arr3)).toBe('A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y & Z');
+  });
 });
