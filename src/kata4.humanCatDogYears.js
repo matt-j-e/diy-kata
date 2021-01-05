c84c3914e8594e8de1e0030cada643cbf735375a
mattedwards <the.matt.edwards@googlemail.com>
2021-01-05 08:43:57 +0000

    completed kata4 humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,17 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    if(!Number.isInteger(number) || number < 0) return "Error. Needs to be a non-negative integer";
 
+    const catYears = calcAge(number, 'cat');
+    const dogYears = calcAge(number, 'dog');
+    return [number, catYears, dogYears];
+};
+
+function calcAge(humanAge, animal) {
+    if(humanAge === 0) return 0;
+    if(humanAge === 1) return 15;
+    if(humanAge === 2) return 15 + 9;
+    if(animal === 'cat') return 24 + ((humanAge - 2) * 4);
+    if(animal === 'dog') return 24 + ((humanAge - 2) * 5);
+}
+    
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,20 @@
+const { TestScheduler } = require("jest");
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+
+describe("Convert human years to cat & dog equivalents", () => {
+    it("returns array of human, cat and dog years when passed human years", () => {
+        expect(humanCatDogYears(10)).toStrictEqual([ 10, 56, 64 ]);
+        expect(humanCatDogYears(0)).toStrictEqual([ 0, 0, 0 ]);
+        expect(humanCatDogYears(1)).toStrictEqual([ 1, 15, 15 ]);
+        expect(humanCatDogYears(2)).toStrictEqual([ 2, 24, 24 ]);
+        expect(humanCatDogYears(3)).toStrictEqual([ 3, 28, 29 ]);
+        expect(humanCatDogYears(54)).toStrictEqual([ 54, 232, 284 ]);
+    });
+
+    it("returns an error when the value passed in isn't a non-negative integer", () => {
+        expect(humanCatDogYears("brocolli")).toBe("Error. Needs to be a non-negative integer");
+        expect(humanCatDogYears("12345")).toBe("Error. Needs to be a non-negative integer");
+        expect(humanCatDogYears(-10)).toBe("Error. Needs to be a non-negative integer");
+    });
+});
