3f8ab55696cbc5323355ecf9787c96eafdadf3f0
mattedwards <the.matt.edwards@googlemail.com>
2021-01-05 19:59:47 +0000

    completed kata5 reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const eta = Math.round(distance / speed * 2) / 2;
+    const unit = eta === 1 ? 'hour' : 'hours';
+    return `I should be there in ${eta} ${unit}.`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,11 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(101, 16)).toBe("I should be there in 6.5 hours.");
+    expect(reachDestination(26, 16)).toBe("I should be there in 1.5 hours.");
+    expect(reachDestination(50, 23)).toBe("I should be there in 2 hours.");
+    expect(reachDestination(9, 10)).toBe("I should be there in 1 hour.");
+  });
 });
